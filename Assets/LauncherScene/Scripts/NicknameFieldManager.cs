28b1090e3fbe5a77aab05a21214a19baf2106ce6
Никита Тихоненко < >
2017-02-14 00:47:34 +0300

    Moved launcher scripts to launcher folder, added NicknameFieldManager script with stubs

---
+++ Assets/LauncherScene/Scripts/NicknameFieldManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(InputField))]
+public class NicknameFieldManager : MonoBehaviour {
+
+    #region Public Fields
+    public static string StandartName = "Too lazy person";
+    #endregion
+
+    #region Public methods
+    public void setNickname(string newNickname) {
+        throw new System.NotImplementedException();
+    }
+    #endregion
+}
+
