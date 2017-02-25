cbc4333359e20edec001be62433aafccbc4838c2
dhoine <hyperwiley@gmail.com>
2017-02-25 22:10:44 +0300

    Crystalls and BaseUnits with VS

---
+++ Assets/Scenes/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    //Все ВС в игре
+    public List<Suprime> suprimes = new List<Suprime>();
+    //Все кристаллы в игре
+    public List<Crystall> crystall = new List<Crystall>();
+
+    private void Start() {
+        SpawnCrystalls();
+        SpawnUnits();
+    }
+    //Создание юнитов и расположение их на начальных точках
+    void SpawnUnits() {
+      
+    }
+    //Создание кристаллов и расположение их на начальных точках
+    void SpawnCrystalls() {
+
+    }
+
+	
+}
