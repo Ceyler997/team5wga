ae6ab75bdd06ff317615fba64dc9add98db99384
Никита Тихоненко < >
2017-02-26 03:24:29 +0300

    some bugs fixed, added TODO, some comments

--- Assets/LauncherScene/Scripts/GameManagerScript.cs
+++ Assets/LauncherScene/Scripts/GameManagerScript.cs
@@ -24,7 +24,7 @@ public class GameManagerScript : Photon.PunBehaviour {
     #endregion
 
     #region Private Fields
-    private bool isConnecting = false; //TODO: check, will it be working in Awake()
+    private bool isConnecting;
     private Text messageText;
     #endregion
 
@@ -34,6 +34,8 @@ public class GameManagerScript : Photon.PunBehaviour {
         PhotonNetwork.logLevel = logLevel;//Setting log level
         PhotonNetwork.autoJoinLobby = true;//joining to lobby on connecting
         PhotonNetwork.automaticallySyncScene = true;//All players in the room will be loading same scene as master
+        //Joined lobby message called if we switched from another scene, so we need to check, is player pressed button
+        isConnecting = false;
 
         messageText = progressPanel.GetComponentInChildren<Text>();//TODO: check, should I use it
         Debug.Log("GameManager is awoken");
@@ -64,7 +66,7 @@ public class GameManagerScript : Photon.PunBehaviour {
 
     #region Connection messages
 
-    public override void OnFailedToConnectToPhoton(DisconnectCause cause) {
+    public override void OnFailedToConnectToPhoton(DisconnectCause cause) {//TODO: pop-up
         Debug.LogError("Failed to connect due to " + cause.ToString());
 
         messageText.text = "Failed to connect due to " + cause.ToString();//TODO: check localisation
@@ -72,6 +74,8 @@ public class GameManagerScript : Photon.PunBehaviour {
 
     public override void OnDisconnectedFromPhoton() {
         Debug.Log("Disconnected from PUN");
+
+        messageText.text = "Diconnected";//TODO: check localisation
     }
     #endregion
 
@@ -86,8 +90,8 @@ public class GameManagerScript : Photon.PunBehaviour {
             messageText.text = "Ready to play!";//TODO: check localisation
         }
     }
-
-    public override void OnPhotonCreateRoomFailed(object [] codeAndMsg) {
+    
+    public override void OnPhotonCreateRoomFailed(object [] codeAndMsg) {//TODO: pop-up
         Debug.LogError("Failed to create room:\n" +
             codeAndMsg [0].ToString() + " " + codeAndMsg [1].ToString());
 
@@ -102,7 +106,7 @@ public class GameManagerScript : Photon.PunBehaviour {
 
         PhotonNetwork.CreateRoom(
             "",//Name will be generated automatically
-            new RoomOptions() { MaxPlayers = 2 },//ATTENTION: hardcoded value
+            new RoomOptions() { MaxPlayers = 2 },//TODO: ATTENTION: hardcoded value
             null);//lobby for room, null for current lobby
     }
 
@@ -114,21 +118,22 @@ public class GameManagerScript : Photon.PunBehaviour {
     #region Players messages
 
     public override void OnPhotonPlayerConnected(PhotonPlayer newPlayer) {//new player entered the room
-        Debug.Log("Second player connected");
+        Debug.Log("Player " + newPlayer.NickName + " connected");
 
         messageText.text = "Ready to play!";//TODO: check localisation
         PhotonNetwork.LoadLevel(battleSceneName);//TODO: create this scene
     }
 
     public override void OnPhotonPlayerDisconnected(PhotonPlayer otherPlayer) {//player left the room
-        Debug.Log("Second player disconnected");
+        Debug.Log("Player " + otherPlayer.NickName + " disconnected");
+        CancelConnection();
         PhotonNetwork.LoadLevel(0);//restarting game in case of disconnection while loading level
     }
     #endregion
 
     #region Server messages
-
-    public override void OnPhotonMaxCccuReached() {//too many peoples connected to server for this subscribe 
+    //NOT TESTED
+    public override void OnPhotonMaxCccuReached() {//too many peoples connected to server for this subscribe plan
         Debug.Log("Server is overloaded");
 
         messageText.text = "Please, wait. Server is overloaded.";//TODO: check localisation
@@ -158,8 +163,9 @@ public class GameManagerScript : Photon.PunBehaviour {
     public void CancelConnection() {
         Debug.Log("Canceling connection");
         isConnecting = false;
-        if (PhotonNetwork.connected)
+        if (PhotonNetwork.connected) {
             PhotonNetwork.Disconnect();
+        }
 
         controlPanel.SetActive(true);
         progressPanel.SetActive(false);
