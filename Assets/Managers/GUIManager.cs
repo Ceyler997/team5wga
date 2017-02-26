98282ddb74d46be0d68b252815da48551d765d8b
Никита Тихоненко < >
2017-02-26 12:59:05 +0300

    Added GUI Manager

---
+++ Assets/Managers/GUIManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public static class GUIManager {
+
+	static void showCanvasGroup(CanvasGroup group) {
+        group.alpha = 1;
+        group.interactable = true;
+        group.blocksRaycasts = true;
+    }
+
+    static void hideCanvasGroup(CanvasGroup group) {
+        group.alpha = 0;
+        group.interactable = false;
+        group.blocksRaycasts = false;
+    }
+}
