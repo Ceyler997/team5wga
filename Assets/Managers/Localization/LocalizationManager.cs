8c7c2b2361b93f5d45b49f7fd68b6cfb76adc845
Никита Тихоненко < >
2017-02-28 13:19:29 +0300

    Button localized

--- Assets/Managers/Localization/LocalizationManager.cs
+++ Assets/Managers/Localization/LocalizationManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 
-public class LocalizationManager: MonoBehaviour {
+public class LocalizationManager: MonoBehaviour {//TODO: move to static whole class
 
     #region Public Fields
 
@@ -12,7 +12,7 @@ public class LocalizationManager: MonoBehaviour {
     #endregion
 
     #region Private Fields
-    private Dictionary<string, string> localizedText;
+    private static Dictionary<string, string> localizedText;
     #endregion
 
     #region MonoBehaviour Methods
@@ -43,6 +43,22 @@ public class LocalizationManager: MonoBehaviour {
         } else {
             Debug.LogError("No such localization!");
         }
+    }
+
+    public static string getTextByKey(string key) {
+        string result;
+
+        if(localizedText == null) {
+            Debug.LogError("Localization not loaded!");
+            return "NOT LOADED";
+        }
+
+        if (localizedText.TryGetValue(key, out result) == false) {
+            Debug.LogError("No localization data for " + key + " key");
+            return "NO DATA";
+        }
+
+        return result;
     }
     #endregion
 }
---
+++ Assets/Managers/Localization/LocalizationText.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LocalizationText : MonoBehaviour {
+
+    #region Public Fields
+
+    [Tooltip("Text field for localization")]
+    public Text textField;
+
+    [Tooltip("Key value for localization")]
+    public string key;
+    #endregion
+
+    #region MonoBehaviour Methods
+    
+    void Start() {
+        UpdateText();
+	}
+    #endregion
+
+    #region Public Methods
+
+    public void UpdateText() {
+        textField.text = LocalizationManager.getTextByKey(key);
+    }
+    #endregion
+}
