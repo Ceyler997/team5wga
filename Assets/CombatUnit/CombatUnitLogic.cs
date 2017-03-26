b4ece0af76512fa7ef410aa72d963ec46999a077
Ceyler997 <Ceyler997@gmail.com>
2017-03-26 23:01:30 +0300

    Added unit scene

---
+++ Assets/CombatUnit/CombatUnitLogic.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatUnitLogic : MonoBehaviour {
+
+    #region Private Fields
+    private 
+    #endregion
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
