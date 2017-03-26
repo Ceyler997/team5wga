0aa4e79411cffe109f6b8e23e6e0e736088d92b8
Ceyler997 <Ceyler997@gmail.com>
2017-03-27 02:56:51 +0300

    Added Magic class, AIBehaviour moved to the folder

---
+++ Assets/CombatUnit/AIBehaviour/AIBehaviour.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System;
+
+abstract class AIBehaviour {
+    abstract public BaseObject Subject {get;set;}
+
+    abstract public void UpdateState();
+}
---
+++ Assets/CombatUnit/Magic/Magic.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Magic : MonoBehaviour {
+
+    #region private fields
+
+    float energyToCast;
+    float castingTime;
+    float durationTime;
+    float currentDurationTime;
+    bool isAbleToCast;
+    #endregion
+
+    #region public methods
+
+    public abstract void cast();
+    public abstract void decast();
+    #endregion
+}
