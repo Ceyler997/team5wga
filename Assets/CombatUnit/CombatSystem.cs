cbb4028dd60a71d810c57d378cdece2cb20a4949
Ceyler997 <Ceyler997@gmail.com>
2017-03-27 02:14:18 +0300

    Added Combat System stab component

--- Assets/CombatUnit/CombatSystem.cs
+++ Assets/CombatUnit/CombatSystem.cs
@@ -4,13 +4,32 @@ using UnityEngine;
 
 public class CombatSystem : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    #region private variables
+
+    float damage;
+    float attackSpeed;
+    float attackRadius;
+    BaseObject target;
+
+    public BaseObject Target {
+        get {
+            return target;
+        }
+
+        set {
+            target = value;
+        }
+    }
+    #endregion
+
+    #region public methods
+
+    public bool attack() {
+        throw new System.NotImplementedException();
+    }
+
+    public void attacked() {
+        throw new System.NotImplementedException();
+    }
+    #endregion
 }
