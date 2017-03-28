59d81c29aff90213419367e608d8462a3e17206c
Ceyler997 <Ceyler997@gmail.com>
2017-03-29 01:25:26 +0300

    Added GameExceptions script (with exceptions for game)

---
+++ Assets/CombatUnit/GameExceptions.cs
@@ -0,0 +1,5 @@
+﻿using System;
+
+class UnitHaveNoMasterException : Exception{ } // Exception in case unit have no master
+
+class NoObjectsInsideRadiusException : Exception { } // Exception in case trying to get object in the radius, when there is no objects inside
