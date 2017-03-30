c234c96b12a9b04f23b86ef8163d42fb2de631a3
Ceyler997 <Ceyler997@gmail.com>
2017-03-30 14:09:22 +0300

    Implemented protective behaviour

--- Assets/CombatUnit/AIBehaviour/UnitProtectiveBehaviour.cs
+++ Assets/CombatUnit/AIBehaviour/UnitProtectiveBehaviour.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using UnityEngine;
 
 public class UnitProtectiveBehaviour : UnitAIBehaviour {
 
     #region private fields
 
     private UnitState currentUnitState;
+    private int agroRadius; // radius from PROTECT TARGET, where unit will attack the enemy
     private BaseObject protectTarget;
     #endregion
 
@@ -36,20 +37,57 @@ public class UnitProtectiveBehaviour : UnitAIBehaviour {
             throw new NoTargetToProtectException();
         }
 
+        CombatSystem cs = Subject.CombatSystem;
+
         switch (CurrentUnitState) {
             case UnitState.CALM:
-                // TODO Implement
+
+                if (cs.IsUnderAttack) {
+                    if (cs.attack()) {
+                        cs.Target.getCombatSystem().attacked(Subject);
+                    }
+                    cs.IsUnderAttack = false; // To not go to loop for two defensive units
+                    return;
+                }
+
+                if (ProtectTarget.RadiusStub.Length != 0) {
+                    IFightable closestEnemy = ProtectTarget.getClosestUnitStub();
+                    if (Vector3.Distance(ProtectTarget.getPosition(), closestEnemy.getPosition()) < agroRadius) {
+                        cs.Target = closestEnemy;
+                        CurrentUnitState = UnitState.ALARMED;
+                        return;
+                    }
+                }
+
+                Subject.MovementAgent.follow(ProtectTarget);
                 break;
+
             case UnitState.ALARMED:
-                // TODO Implement
+                
+                if(ProtectTarget.RadiusStub.Length == 0) {
+                    CurrentUnitState = UnitState.CALM;
+                    return;
+                }
+
+                if (!cs.IsUnderAttack) {
+                    cs.Target = protectTarget.getClosestUnitStub();
+                }
+
+                if (cs.attack()) {
+                    cs.Target.getCombatSystem().attacked(Subject);
+                } else {
+                    Subject.MovementAgent.moveTo(cs.Target.getPosition());
+                }
+
                 break;
+
             default:
                 throw new UndefinedDefensiveUnitStateException();
         }
     }
 }
 
-enum UnitState {
+public enum UnitState {
     CALM,
     ALARMED
 }
