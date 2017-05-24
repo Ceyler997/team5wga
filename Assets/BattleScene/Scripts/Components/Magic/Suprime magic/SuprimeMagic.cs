4f815f042ba2aa8d40191001aff8892e67ec1c52
Ceyler997 <Ceyler997@gmail.com>
2017-05-24 09:41:21 +0300

    reordering in folders

---
+++ Assets/BattleScene/Scripts/Components/Magic/Suprime magic/SuprimeMagic.cs
@@ -0,0 +1,14 @@
+﻿
+public abstract class SuprimeMagic : Magic {
+
+	float castRadius; // максимальное расстояние до цели
+
+    protected bool IsCasting {
+        get { return Caster.Magic.IsCasting; }
+    }
+
+    protected bool CanNotRun() {
+        // Если суприм побежал, то сбрасываем каст
+        return !Caster.MoveSystem.IsFinishedMovement;
+    }
+}
