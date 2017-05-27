15b69ff8d1d6c13ed7ec85af34f08001f46458b0
Ceyler997 <Ceyler997@gmail.com>
2017-05-27 23:31:19 +0300

    Added crystall ui

---
+++ Assets/BattleScene/Scripts/UI/CrystalUI.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class CrystalUI : MonoBehaviour {
+
+    public Sprite enemyCrystalImage;
+    public Sprite neutralCrystalImage;
+    public Sprite frienlyCrystalImage;
+    public Crystal observedCrystal;
+
+    private Image CrystalImage { get; set; }
+    private Player CrystalOwner { get; set; }
+
+    private void Start() {
+        CrystalImage = GetComponent<Image>();
+        CrystalOwner = null;
+        CrystalImage.sprite = neutralCrystalImage;
+    }
+
+    private void Update() {
+        if (CrystalOwner != observedCrystal.ControllingPlayer) {
+            CrystalOwner = observedCrystal.ControllingPlayer;
+
+            if (CrystalOwner == null) {
+                CrystalImage.sprite = neutralCrystalImage;
+            } else if (CrystalOwner.ID == PhotonNetwork.player.ID) {
+                CrystalImage.sprite = frienlyCrystalImage;
+            } else {
+                CrystalImage.sprite = enemyCrystalImage;
+            }
+        }
+    }
+}
