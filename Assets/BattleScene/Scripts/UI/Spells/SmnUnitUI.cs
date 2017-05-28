85e542ac4ba47b3e328fc964048a3abee4f16f71
Ceyler997 <Ceyler997@gmail.com>
2017-05-28 06:33:01 +0300

    Added cat summon

---
+++ Assets/BattleScene/Scripts/UI/Spells/SmnUnitUI.cs
@@ -0,0 +1,16 @@
+﻿
+public class SmnUnitUI : SpellUI {
+    protected override void Update() {
+        ControllableUnit unit = CharactersController.Instance.SelectedUnit;
+
+        if (unit != null) {
+            Suprime suprime = unit.Subject;
+            Spell = suprime.Magic.SmnUnits;
+        } else {
+            Spell = null;
+        }
+
+        base.Update();
+    }
+}
+
